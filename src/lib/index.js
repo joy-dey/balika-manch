fd726a280412668e3daaf9e907cceb26a0f6f4f8
joydey <joy-dey@outlook.com>
2025-06-27 14:29:27 +0530

    feat(lib): import Footer component and update exports in index.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,4 @@
 // place files you want to import through the `$lib` alias in this folder.
 import Navbar from './components/Navbar.svelte';
-export { Navbar };
+import Footer from './components/Footer.svelte';
+export { Navbar, Footer };
